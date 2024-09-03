955aebd747901995b32fa8d5ad51708790c63ffc
MarkL <108855073+MarkLumaino@users.noreply.github.com>
2024-09-03 13:52:02 +0800

    responsive-text-cover

--- layouts/default.vue
+++ layouts/default.vue
@@ -2,7 +2,7 @@
     <div style="background-image: url('/bg-image.png');background-size:contain;" >
         <HeaderTop class="fixed top-0 left-0 w-full z-50" />
         <HeaderNavigation  class="fixed top-0 left-0 w-full z-50 mt-12"/>
-        <div class="content-wrapper mx-16">
+        <div class="content-wrapper">
         <slot></slot>
         </div>
         <FooterSection />
