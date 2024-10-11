9d8a783bb13b788f9e01d7165cc5f326332b8deb
MarkL <108855073+MarkLumaino@users.noreply.github.com>
2024-10-11 13:34:52 +0800

    reservation page index

---
+++ pages/reservation/index.vue
@@ -0,0 +1,27 @@
+<template>
+       <div class="flex flex-column align-center">
+        <SectionsReservationsReservationDetails/>
+       <v-btn
+        :loading="loading"
+        class="flex-grow-1 mb-11"
+        width="400"
+        height="48"
+        variant="tonal"
+        @click="load"
+      >
+        Proceed
+      </v-btn>
+       </div>
+</template>
+<script>
+  export default {
+    data: () => ({ loading: false }),
+
+    methods: {
+      load () {
+        this.loading = true
+        setTimeout(() => (this.loading = false), 3000)
+      },
+    },
+  }
+</script>
