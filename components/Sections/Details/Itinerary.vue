0cdc5b8ad5a7c15d98c9dbe50f42d92c9d1b3fcb
Arneil Paul Polican <policanarneilpaul@gmail.com>
2024-12-06 17:44:09 +0800

    details

--- components/Sections/Details/Itinerary.vue
+++ components/Sections/Details/Itinerary.vue
@@ -1,10 +1,13 @@
 <template>
     <div>
         <div class="flex flex-row gap-4 my-2 mt-16">
-            <div class="w-1/2 ">
+            <div class="w-full ">
                 <div class="flex justify-between">
                     <div>
                         <h1 class="font-bold" style="font-size: 42px;">ITINERARY</h1>
+                        <br/>
+                        <p v-if="details">{{ details.description }}</p>
+                        <p v-else>No details available.</p>
                     </div>
                 </div>
             </div>
@@ -13,4 +16,12 @@
 </template>
 
 <script>
+export default {
+    props: {
+        details: {
+            type: Object, // or String/Array based on the data type of details_raw
+            required: true, // Set to `true` if this prop is mandatory
+        },
+    },
+};
 </script>
